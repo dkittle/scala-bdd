8c0ab1f954025b679cc6781843bafb73e91c9495
dkittle <don@kittle.ca>
2015-10-07 17:46:43 -0400

    more idiomatic impl by Abhijit Singh

--- src/test/scala/ca/kittle/util/SeleniumShared.scala
+++ src/test/scala/ca/kittle/util/SeleniumShared.scala
@@ -4,18 +4,8 @@ import org.scalatest.selenium.Page
 
 trait SeleniumShared extends Page with DriverSelector with Configurator {
 
-  def clickOnLinkByText(text: String): Boolean = {
-    Option(find(linkText(text)))  match {
-      case None => false
-      case _ => click on linkText(text); true
-    }
-  }
+  def clickOnLinkByText(text: String): Boolean = find(linkText(text)).map(_ => click on linkText(text)).isDefined
 
-  def clickOnLinkById(text: String): Boolean = {
-    Option(find(id(text)))  match {
-      case None => false
-      case _ => click on id(text); true
-    }
-  }
+  def clickOnLinkById(text: String): Boolean = find(id(text)).map (_ => click on id(text)).isDefined
 
 }
